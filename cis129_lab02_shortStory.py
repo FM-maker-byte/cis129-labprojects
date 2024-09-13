c9954a57347473559ebbe3bf62d40e684fe0dba7
FM-maker-byte <fmoses1@mail.pima.edu>
2024-09-12 21:50:21 -0700

    First Python lab with short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Faith Moses
+# My first introductory GitHub Python lab in my CIS129 course. 
+# Printed a short story of a bird. 
+print("The Bird and the Ocean")
+print("There was once a blue bird that flew to the sky.")
+print("It gravitated to the heavens, wanting to be apart of it.")
+print("Curiosity made it venture far out. Until the sky turned into water.")
+print("It emerged into a new world parallel his own.")
