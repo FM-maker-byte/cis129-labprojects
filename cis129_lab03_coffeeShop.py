0eb4f0e68a1d1b8b9f25f150f6873a47df45c6ff
FM-maker-byte <fmoses1@mail.pima.edu>
2024-09-19 17:49:33 -0700

    Update cis129_lab03_coffeeShop.py
    code was altered, needed to manually insert with conflict

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -2,33 +2,62 @@
 #CIS129
 #Creator: Faith Moses
 
-#Getting input
-print('What are you purchasing?')
-nameOfItem = input('Name of Item 1')
-nameOfItem_2 = input('Name of Item 2')
+# What are you purchasing
+nameOfItem = 'Coffees'
+nameOfItem_2 = 'Muffins'
+nameOfItem_3 = 'Tea'
+nameOfItem_4 = 'Bagels'
+
+#Getting Input
+
 print('Please enter the number of items bought')
 numberOfItemsPurchased = int(input('Number of Coffees Purchased'))
 numberOfItemsPurchased_2 = int(input('Number of Muffins Purchased'))
+numberOfItemsPurchased_3 = int(input('Number of Tea Purchased'))
+numberOfItemsPurchased_4 = int(input('Number of Bagels Purchased'))
+
 print('Please enter the price of the item')
 priceOfItem = int(input('Price of Coffee'))
-priceOfItem_2 = int(input('Price of Muffin'))
+priceOfItem_2 = int(input('Price of Muffins'))
+priceOfItem_3 = int(input('Price of Tea'))
+priceOfItem_4 = int(input('Price of Bagels'))
+
 coffeeTotalCost = numberOfItemsPurchased * priceOfItem
 muffinTotalCost = numberOfItemsPurchased_2 * priceOfItem_2
-taxOfTotal = (coffeeTotalCost + muffinTotalCost) * 0.06
+teaTotalCost = numberOfItemsPurchased_3 * priceOfItem_3
+bagelTotalCost = numberOfItemsPurchased_4 * priceOfItem_4
+
+
+taxOfTotal = (coffeeTotalCost + muffinTotalCost
+               + teaTotalCost +bagelTotalCost) * 0.06
+
 #Display Receipt to Customer
 print('***************************************************')
-print('        My Coffee and Muffin Shop')
-print('Number of Coffees bought?', numberOfItemsPurchased)
+print('        My Breakfast Shop')
+print('Number of coffees bought?', numberOfItemsPurchased)
 print('Number of Muffins bought?', numberOfItemsPurchased_2)
+print('Number of Teas bought?', numberOfItemsPurchased_3)
+print('Number of Bagels bought?', numberOfItemsPurchased_4)
+
+
 print('*****************************************************')
 print('*****************************************************')
-print('My Coffee and Muffin Shop Receipt')
-print('1 Coffee at $5 each:         $', coffeeTotalCost)
-print('2 Muffins at $4 each:        $', muffinTotalCost)
+print('My Breakfast Shop Receipt')
+print(numberOfItemsPurchased, nameOfItem, 'at',
+      priceOfItem, 'each:         $', coffeeTotalCost)
+print(numberOfItemsPurchased_2, nameOfItem_2, 'at',
+      priceOfItem_2, 'each:         $', muffinTotalCost)
+print(numberOfItemsPurchased_3, nameOfItem_3, 'at',
+      priceOfItem_3, 'each:             $', teaTotalCost)
+print(numberOfItemsPurchased_4, nameOfItem_4, 'at',
+      priceOfItem_4, 'each:          $', bagelTotalCost)
+
+
 print('6% tax:                      $', taxOfTotal)
 print('                           ----------------------')
-print('Total:                       $', (coffeeTotalCost + muffinTotalCost 
-                                         + taxOfTotal))
-print('*****************************************************')
-
-#Further customization can be done for the receipt, including the price and layout. 
+print('Total:                     $', (coffeeTotalCost + muffinTotalCost 
+                                       + teaTotalCost + bagelTotalCost 
+                                       + taxOfTotal))
+print('Thank you for shopping at our Breakfast Shop!',
+      'Have a Wonderful day!')
+print('*****************************************************') 
