99acb25bc131c704dc16951f1dfecf63ead30a11
FM-maker-byte <fmoses1@mail.pima.edu>
2024-11-14 08:06:27 -0700

    Lab 11 Class average pt.2
    updated some additional functional information for 9.3 section

--- CIS129_FaithMoses_Lab11.1.py
+++ CIS129_FaithMoses_Lab11.1.py
@@ -5,23 +5,20 @@
 
 import csv                 # importing csv module from python library
 
-def getStudentData():      # function to get the student data input
-    firstName = input("Enter student's first name: ")
-    lastName =  input("Enter student's last name: ")
-    exam1Grade = input('Exam 1 grade: ')
-    exam2Grade = input ('Exam 2 grade: ')
-    exam3Grade = input('Exam 3 grade: ')
-    studentData = [firstName, lastName, exam1Grade, exam2Grade, exam3Grade]
-    return studentData
+# The section to write csv grade file using student data input
+fileGrade = 'grades.csv'
+with open(fileGrade, mode='w', newline='') as file:      # writing a new csv file for student data
+    while True:                                          # looping student data input while yes
+        firstName = input("Enter student's first name: ") # getting input data from the student
+        lastName =  input("Enter student's last name: ")
+        exam1Grade = input('Exam 1 grade: ')
+        exam2Grade = input ('Exam 2 grade: ')
+        exam3Grade = input('Exam 3 grade: ')
+        studentData = [firstName, lastName, exam1Grade, exam2Grade, exam3Grade]
+        repeat = input("Do you want to add another student's info? (yes/no): ")
+        if repeat.casefold() == 'yes':
+            writer = csv.writer(file)
+            writer.writerow(studentData)
+        else: 
+            break
 
-def write_grades_CSV(studentData):     # function to write csv grade file using student data input
-    fileGrade = 'grades.csv'
-    with open(fileGrade, mode='w', newline='') as file:      # writing a new csv file for student data
-        while True:                                          # looping student data input while yes
-            getStudentData()
-            repeat = input("Do you want to add another student's info? (yes/no): ")
-            if repeat.casefold() == 'yes':
-                writer = csv.writer(file)
-                writer.writerow(studentData)
-            else: 
-                break
